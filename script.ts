bd7a11965f58fdc1b58afd1bffeb3367200defd6
Komodo <noreply@komodo.ar>
2025-09-15 13:10:02 -0300

    feat: 2d grid

--- script.ts
+++ script.ts
@@ -1,49 +1,72 @@
 const grid = document.getElementById("grid")!;
 
-setProp(grid, "color-a", "red");
-setProp(grid, "color-b", "blue");
-setProp(grid, "len", 10);
+const width = 5;
+const height = 5;
+
+setProp(grid, "color-x1", "#f00");
+setProp(grid, "color-x2", "#0f0");
+setProp(grid, "color-y1", "#00f");
+setProp(grid, "color-y2", "#fff");
+setProp(grid, "width", width);
+setProp(grid, "height", height);
 
 let selected: HTMLElement | undefined;
 
-const len = 10;
-for (let x = 0; x < len; x++) {
-  const swatch = document.createElement("div");
-  swatch.className = "swatch";
-  setProp(swatch, "x", x / (len - 1));
-  setProp(swatch, "color-x", x / (len - 1));
-  swatch.addEventListener("click", () => {
-    if (selected) {
-      const x = parseFloat(getProp(swatch, "x"));
-      setProp(swatch, "x", getProp(selected, "x"));
-      setProp(selected, "x", x);
-      selected.classList.remove("selected");
-      selected = undefined;
-      if (isSolved()) {
-        const win = document.createElement("h1");
-        win.textContent = "WIN!!!";
-        document.body.appendChild(win);
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
+    const swatch = document.createElement("div");
+    swatch.className = "swatch";
+    setProp(swatch, "x", x / (width - 1));
+    setProp(swatch, "color-x", x / (width - 1));
+    setProp(swatch, "y", y / (height - 1));
+    setProp(swatch, "color-y", y / (height - 1));
+    swatch.addEventListener("click", () => {
+      if (selected) {
+        const x = parseFloat(getProp(swatch, "x"));
+        const y = parseFloat(getProp(swatch, "y"));
+        setProp(swatch, "x", getProp(selected, "x"));
+        setProp(swatch, "y", getProp(selected, "y"));
+        setProp(selected, "x", x);
+        setProp(selected, "y", y);
+        selected.classList.remove("selected");
+        selected.classList.add("moving");
+        swatch.classList.add("moving");
+        selected = undefined;
+        if (isSolved()) {
+          const win = document.createElement("h1");
+          win.textContent = "WIN!!!";
+          document.body.appendChild(win);
+        }
+      } else {
+        swatch.classList.add("selected");
+        selected = swatch;
       }
-    } else {
-      swatch.classList.add("selected");
-      selected = swatch;
-    }
-  });
-  grid.appendChild(swatch);
+    });
+    swatch.addEventListener("transitionend", () => {
+      swatch.classList.remove("moving");
+    });
+    grid.appendChild(swatch);
+  }
 }
 
 for (let i = 0; i < grid.childElementCount - 1; i++) {
   const j = Math.floor(Math.random() * (grid.childElementCount - i) + i);
   const a = grid.children[i] as HTMLElement;
   const b = grid.children[j] as HTMLElement;
-  const tmp = getProp(a, "x");
+  const tmp_x = getProp(a, "x");
+  const tmp_y = getProp(a, "y");
   setProp(a, "x", getProp(b, "x"));
-  setProp(b, "x", tmp);
+  setProp(a, "y", getProp(b, "y"));
+  setProp(b, "x", tmp_x);
+  setProp(b, "y", tmp_y);
 }
 
 function isSolved(): boolean {
   for (const swatch of Array.from(grid.children) as HTMLElement[]) {
-    if (getProp(swatch, "x") !== getProp(swatch, "color-x")) {
+    if (
+      getProp(swatch, "x") !== getProp(swatch, "color-x") ||
+      getProp(swatch, "y") !== getProp(swatch, "color-y")
+    ) {
       return false;
     }
   }
